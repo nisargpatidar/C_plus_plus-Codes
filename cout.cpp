33e0c4f20374051635ad6743a2923c1add168a61
333 NisargPatel 333 <56943727+nisargpatidar@users.noreply.github.com>
2022-02-11 23:31:50 -0600

    Create cout.cpp

---
+++ cout.cpp
@@ -0,0 +1,14 @@
+//Here we declare all the header files, to declare the header files we use #include <header_file_name>
+#include <iostream>
+//We are using "using namespace std", so that in the code we don't need to use std::cout everytime
+using namespace std;
+
+//Here we declare the main function with the int data type so we close the program by return and exit code 0
+int main()
+{
+  
+  //Here we use the cout function to print the output to the console, whatever we write in between the "___", will be printed as it is, the only exception is that '\' is considered as escape sequences
+  cout << "This is the progam to see the use of cout function";
+ 
+  return 0;
+}
